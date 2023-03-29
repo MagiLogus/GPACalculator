4c8a5a3a213175a8fbd0447dae7449eea8781d80
Paulo Henrique Gonçalves de Oliveira <paulo.oliviera.phgo@gmail.com>
2023-03-29 16:37:39 -0300

    realizado a construção de uma calculadora de media aritmetica com c#

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Crie um programa para calcular a media aritmética 
+
+// Algoritimo 
+// Entrada: as 5 notas
+// Proscessamento: somar as 5 notas e dividir por 5
+
+//Entrada 
+Console.BackgroundColor =ConsoleColor.Black;
+
+Console.WriteLine(@$"
+--------------------------------
+|       Programa para          |
+|     calcular sua media       |
+|         Aritmética           |
+--------------------------------
+");
+
+Console.WriteLine($"Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Bem vindo {nome} !");
+
+Console.WriteLine($"Digite sua nota 1: ");
+float nota1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 2: ");
+float nota2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 3: ");
+float nota3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 4: ");
+float nota4 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 5: ");
+float nota5 = float.Parse(Console.ReadLine());
+
+// Processamento 
+float media = (nota1+nota2+nota3+nota4+nota5)/5;
+
+// Saida
+Console.WriteLine($"{nome} sua media é : {media} pontos. ");
+
+Console.WriteLine($"Obrigado por utilizar nosso sistema. ");
